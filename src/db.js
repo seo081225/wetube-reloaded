1b785acfba6dfaffaea2d8053b137502ab3ae5f5
seo <seo081225@gmail.com>
2023-02-13 16:16:51 +0900

    aa

--- src/db.js
+++ src/db.js
@@ -2,14 +2,14 @@ import mongoose from "mongoose";
 
 mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
     useCreateIndex: true,
 });
 
 const db = mongoose.connection;
 
-const handleOpen = () => console.log("Connected to DB✅");
+const handleOpen = () => console.log("✅Connected to DB");
 const handleError = (error) => console.log("❌DB Error💀", error);
 
 db.on("error", handleError);
