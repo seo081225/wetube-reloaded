0ddfdccb717b66bac63a4c9c047a700f7b7decd9
seo <seo081225@gmail.com>
2023-02-21 17:25:51 +0900

    13.3

---
+++ src/client/js/recorder.js
@@ -0,0 +1,41 @@
+const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
+
+let stream;
+let recorder;
+
+const handleStop = () => {
+    startBtn.innerText = "Start Recording";
+    startBtn.removeEventListener("click", handleStop);
+    startBtn.addEventListener("click", handleStart);
+    recorder.stop();
+};
+
+const handleStart = () => {
+    startBtn.innerText = "Stop Recording";
+    startBtn.removeEventListener("click", handleStart);
+    startBtn.addEventListener("click", handleStop);
+    recorder = new MediaRecorder(stream);
+    recorder.ondataavailable = (event) => {
+        const videoFile = URL.createObjectURL(event.data);
+        console.log(video);
+        video.srcObject = null;
+        video.src = videoFile;
+        video.loop = true;
+        video.play();
+    };
+    recorder.start();
+};
+
+const init = async () => {
+    stream = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: true,
+    });
+    video.srcObject = stream;
+    video.play();
+};
+
+init();
+
+startBtn.addEventListener("click", handleStart);
