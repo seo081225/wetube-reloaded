5007f9e6f5c713a89b979721088262059f7191cd
seo <seo081225@gmail.com>
2023-02-22 17:05:19 +0900

    14.4

--- src/server.js
+++ src/server.js
@@ -42,6 +42,12 @@ app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
+app.use((req, res, next) => {
+    res.header("Cross-Origin-Embedder-Policy", "require-corp");
+    res.header("Cross-Origin-Opener-Policy", "same-origin");
+    next();
+});
+
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 app.use("/api", apiRouter);
