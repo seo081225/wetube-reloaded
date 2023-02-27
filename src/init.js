f1fe7e094d20564464bb86e81a82885ccdc79e42
seo <seo081225@gmail.com>
2023-02-27 17:39:42 +0900

    16.2

--- src/init.js
+++ src/init.js
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import "./db.js";
 import "./models/Video.js";
+import "./models/User.js";
+import "./models/Comment.js";
 import app from "./server.js";
 
 const PORT = 4000;
