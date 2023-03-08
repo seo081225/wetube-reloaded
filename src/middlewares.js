90774cd84a80131917d121befb7e5b46e347c0fc
seo <seo081225@gmail.com>
2023-03-08 21:17:40 +0900

    17.7

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,25 +1,15 @@
 import multer from "multer";
 import multerS3 from "multer-s3";
 import aws from "aws-sdk";
-
 const s3 = new aws.S3({
     credentials: {
         accessKeyId: process.env.AWS_ID,
         secretAccessKey: process.env.AWS_SECRET,
     },
 });
-
-const isHeroku = process.env.NODE_ENV === "production";
-
-const s3ImageUploader = multerS3({
+const multerUploader = multerS3({
     s3: s3,
-    bucket: "wetubeee/images",
-    acl: "public-read",
-});
-
-const s3VideoUploader = multerS3({
-    s3: s3,
-    bucket: "wetubeee/videos",
+    bucket: "seotube",
     acl: "public-read",
 });
 
@@ -54,7 +44,7 @@ export const avatarUpload = multer({
     limits: {
         fileSize: 3000000,
     },
-    storage: isHeroku ? s3ImageUploader : undefined,
+    storage: multerUploader,
 });
 
 export const videoUpload = multer({
@@ -62,5 +52,5 @@ export const videoUpload = multer({
     limits: {
         fileSize: 10000000,
     },
-    storage: isHeroku ? s3VideoUploader : undefined,
+    storage: multerUploader,
 });
