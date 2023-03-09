a8acf552ae3c45cea912d29b7866ae1215a5c96e
seo <seo081225@gmail.com>
2023-03-09 10:22:29 +0900

    17.7...

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,12 +1,17 @@
 import multer from "multer";
 import multerS3 from "multer-s3";
 import aws from "aws-sdk";
+// import { S3Client } from "@aws-sdk/client-s3";
+
 const s3 = new aws.S3({
+    // const s3 = new S3Client({
+    region: "ap-northeast-2",
     credentials: {
         accessKeyId: process.env.AWS_ID,
         secretAccessKey: process.env.AWS_SECRET,
     },
 });
+
 const multerUploader = multerS3({
     s3: s3,
     bucket: "seotube",
